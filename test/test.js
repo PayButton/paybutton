67aac0a772422829c8c68b1eb8b169c80d1c4e99
PayButton <45511090+PayButton@users.noreply.github.com>
2018-12-03 02:51:00 -0600

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,64 +1,55 @@
 $(document).ready(function() {
-alert("document is ready");
-var badgerButtons = document.body.getElementsByClassName("badger-button");
-  for (var i = 0; i < badgerButtons.length; i++) {
-    var badgerButton = badgerButtons[i];
-    var buttonAmount = badgerButton.getAttribute("amount");
-    var amountType = badgerButton.getAttribute("amount-type");
-  }
+    var badgerButtons = document.body.getElementsByClassName("badger-button");
+    for (var i = 0; i < badgerButtons.length; i++) {
+        var badgerButton = badgerButtons[i];
+        var buttonAmount = badgerButton.getAttribute("amount");
+        var amountType = badgerButton.getAttribute("amount-type");
+        var amountType = badgerButton.getAttribute("amount-type");
+        alert(buttonAmount + ' ' + amountType);
+    }
 
-  function getBCHPrice() {
-    return new Promise((resolve, reject) => {
-      jQuery.getJSON("https://index-api.bitcoin.com/api/v0/cash/price/" + amountType, function(result) {
-        if (result.price != "") {
-          var addDecimal = result.price / 100;
-          var pricePersatoshi = addDecimal / 100000000;
-          var satoshiAmount = buttonAmount / pricePersatoshi;
-          resolve(satoshiAmount);
-        } else {
-          reject(new Error(result.error));
-        }
-      });
-    });
-  }
-
-
+    function getBCHPrice() {
+        return new Promise((resolve, reject) => {
+            jQuery.getJSON("https://index-api.bitcoin.com/api/v0/cash/price/" + amountType, function(result) {
+                if (result.price != "") {
+                    var addDecimal = result.price / 100;
+                    var pricePersatoshi = addDecimal / 100000000;
+                    var satoshiAmount = buttonAmount / pricePersatoshi;
+                    resolve(satoshiAmount);
+                } else {
+                    reject(new Error(result.error));
+                }
+            });
+        });
+    }
 
     getBCHPrice().then(function(res) {
 
-
-
-
-      badgerButton.addEventListener("click", function(event) {
-        if (typeof web4bch !== "undefined") {
-          web4bch = new Web4Bch(web4bch.currentProvider);
-
-          var txParams = {
-            to: badgerButton.getAttribute("data-to"),
-            from: web4bch.bch.defaultAccount,
-            value: res
-          };
-
-
-          web4bch.bch.sendTransaction(txParams, (err, res) => {
-            if (err) return;
-            var paywallId = badgerButton.getAttribute("data-paywall-id");
-            if (paywallId) {
-              var paywall = document.getElementById("paywall");
-              paywall.style.display = "block";
+        badgerButton.addEventListener("click", function(event) {
+            if (typeof web4bch !== "undefined") {
+                web4bch = new Web4Bch(web4bch.currentProvider);
+                var txParams = {
+                    to: badgerButton.getAttribute("data-to"),
+                    from: web4bch.bch.defaultAccount,
+                    value: res
+                };
+                web4bch.bch.sendTransaction(txParams, (err, res) => {
+                    if (err) return;
+                    var paywallId = badgerButton.getAttribute("data-paywall-id");
+                    if (paywallId) {
+                        var paywall = document.getElementById("paywall");
+                        paywall.style.display = "block";
+                    }
+                    var successCallback = badgerButton.getAttribute("data-success-callback");
+                    if (successCallback) {
+                        window[successCallback](res);
+                    }
+                });
+            } else {
+                window.open('https://badgerwallet.cash');
             }
-            var successCallback = badgerButton.getAttribute("data-success-callback");
-            if (successCallback) {
-              window[successCallback](res);
-            }
-          });
-
-
-        } else {
-          window.open('https://badgerwallet.cash');
-        }
-      });
+        });
 
     });
 
-    });
+});
