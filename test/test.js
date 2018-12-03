e5815873976e93fb0746ada2e1d45e580a152f03
PayButton <45511090+PayButton@users.noreply.github.com>
2018-12-02 22:12:44 -0600

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
 
 function getBCHPrice () {
   return new Promise((resolve, reject) => {
-    jQuery.getJSON("https://index-api.bitcoin.com/api/v0/cash/price/usd", function (result) {
+    jQuery.getJSON("https://index-api.bitcoin.com/api/v0/cash/price/jpy", function (result) {
       if (result.price != "") {
         var singleDollarValue = result.price / 100;
         var singleDollarSatoshis = 100000000 / singleDollarValue;
