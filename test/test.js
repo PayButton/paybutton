9e7262187b192d3ac0565133309badca3de998b0
PayButton <45511090+PayButton@users.noreply.github.com>
2018-12-03 02:14:16 -0600

    Update test.js

--- test/test.js
+++ test/test.js
@@ -59,15 +59,3 @@
 
     });
   });
-
-  var cssId = 'myCss'; // you could encode the css path itself to generate id..
-  if (!document.getElementById(cssId)) {
-    var head = document.getElementsByTagName('head')[0];
-    var link = document.createElement('link');
-    link.id = cssId;
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = 'https://paybutton.cash/test/test.css';
-    link.media = 'all';
-    head.appendChild(link);
-  }
