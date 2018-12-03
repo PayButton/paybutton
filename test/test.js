cbbe92e6b1232951204b714010f5e3ea79dcbd06
PayButton <45511090+PayButton@users.noreply.github.com>
2018-12-02 23:15:40 -0600

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
 
 function getBCHPrice () {
   return new Promise((resolve, reject) => {
-    jQuery.getJSON("https://index-api.bitcoin.com/api/v0/cash/price/usd", function (result) {
+    jQuery.getJSON("https://index-api.bitcoin.com/api/v0/cash/price/jpy", function (result) {
       if (result.price != "") {
         var buttonAmount = 2
         var addDecimal = result.price / 100;
