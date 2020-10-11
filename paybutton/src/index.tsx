fbe57eb959e5f310cced5ddcb8585d524e7d9454
Paul Oldridge <paulrichardoldridge@gmail.com>
2020-10-11 15:47:19 -0700

    Renamed folders to match npm package names

--- paybutton/src/index.tsx
+++ paybutton/src/index.tsx
@@ -1,7 +1,129 @@
-import * as Components from './components';
+import camelcase from 'camelcase';
+import { PayButton, PayButtonProps, Widget, WidgetProps } from 'paybutton';
+import { Component, h } from 'preact';
+import { render } from 'preact/compat';
 
-export const PayButton = Components.PayButton;
-export type PayButtonProps = Components.PayButtonProps;
+const payButtonApi = {
+  render: ( el: HTMLElement, props: PayButtonProps ) => {
+    render( <PayButton { ...props } />, el )
+  },
+  renderWidget: ( el: HTMLElement, props: WidgetProps ) => {
+    render( <Widget { ...props } />, el )
+  },
+  //openDialog:
+}
 
-export const Widget = Components.Widget;
-export type WidgetProps = Components.WidgetProps;
+declare global {
+  interface Window { 
+    PayButton: typeof payButtonApi;
+    WebKitMutationObserver: any;
+  }
+}
+
+if ( typeof window !== 'undefined' ) {
+  init();
+  window.PayButton = payButtonApi;
+}
+
+function init ( ) {
+  function render ( ) {
+    renderButtons();
+    renderWidgets();
+  }
+
+  document.addEventListener( 'DOMContentLoaded', render );
+
+  const MutationObserver = window.MutationObserver ?? window.WebKitMutationObserver;
+  const observer = new MutationObserver( render );
+  observer.observe( document, { 
+    subtree: true,
+    childList: true,
+    attributes: true,
+  } );
+}
+
+const allowedProps = [
+  'amount',
+  'animation',
+  'currency',
+  'displayCurrency',
+  'hideToasts',
+  'hoverText',
+  'onSuccess',
+  'onTransaction',
+  'randomSatoshis',
+  'successText',
+  'theme',
+  'text',
+  'to',
+];
+
+const requiredProps = [
+  'to',
+];
+
+export function renderButtons ( ): void {
+  findAndRender( 'paybutton', PayButton, allowedProps, requiredProps );
+}
+
+export function renderWidgets ( ): void {
+  findAndRender( 'paybutton-widget', Widget, allowedProps, requiredProps );
+}
+
+function findAndRender <T>( className: string, Component: React.ComponentType<any>, allowedProps: string[], requiredProps: string[] ) {
+  Array
+    .from( document.getElementsByClassName( className ) )
+    .forEach( el => {
+
+      const attributes = el.getAttributeNames( )
+        .reduce( 
+          (attributes: Record<string,string>, name: string) => {
+            const prop = camelcase( name );
+            if ( allowedProps.includes( prop ) ) 
+              attributes[ prop ] = el.getAttribute( name )!;
+            return attributes;
+          }, { } 
+        )
+      ;
+
+      const props: Record<string,any>= Object.assign( { }, attributes, { to: attributes.to } );
+
+      if ( attributes.amount != null )
+        props.amount = +attributes.amount;
+
+      props.hideToasts = attributes.hideToasts === 'true';
+      props.randomSatoshis = attributes.randomSatoshis === 'true';
+
+      if ( attributes.onSuccess ) {
+        const geval = window.eval;
+        props.onSuccess = geval( attributes.onSuccess );
+      }
+
+      if ( attributes.onTransaction ) {
+        const geval = window.eval;
+        props.onTransaction = geval( attributes.onTransaction );
+      }
+
+      if ( attributes.theme ) {
+        try { 
+          props.theme = JSON.parse( attributes.theme )
+        } catch {
+          // Keep the original string assignment
+        }
+      }
+
+      if ( ! requiredProps.every( name => name in attributes ) ) {
+        console.error( 'PayButton: missing required attribute: ' + JSON.stringify( requiredProps.filter( name => ! ( name in attributes ) ) ) );
+        return;
+      }
+
+      el.classList.remove( className );
+
+      render( <Component { ...props } />, el )
+    } );
+}
+
+export default {
+  renderButtons,
+  renderWidgets,
+};
---
+++ react/src/index.tsx
@@ -0,0 +1,7 @@
+import * as Components from './components';
+
+export const PayButton = Components.PayButton;
+export type PayButtonProps = Components.PayButtonProps;
+
+export const Widget = Components.Widget;
+export type WidgetProps = Components.WidgetProps;
