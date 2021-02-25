5d4cbc3625d269707181c5806411e68755a9e4ce
Unknown <paoolkim@gmail.com>
2021-02-24 17:36:03 -0800

    fix string parse

--- react/src/util/format.ts
+++ react/src/util/format.ts
@@ -1,8 +1,10 @@
-export const amount = (x?: number | null): string | undefined =>
-  x
+export const amount = (x?: number | null): string | undefined => {
+  const clean = +x!;
+  return clean
     ?.toFixed(8)
     .replace(/\.0*$/, '')
     .replace(/(\.\d*?)0*$/, '$1');
+};
 
 export default {
   amount,
