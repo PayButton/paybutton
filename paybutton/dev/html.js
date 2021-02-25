2bccbc7b9d3f05556985d77f4f85da95d972271e
Unknown <paoolkim@gmail.com>
2021-02-24 23:02:11 -0800

    dev page styling

--- paybutton/dev/html.js
+++ paybutton/dev/html.js
@@ -4,12 +4,16 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(`${path.join(__dirname, '..', 'dist')}`));
+app.use(express.static(`${path.join(__dirname)}`));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/default.html'));
 });
 app.get('/widget', (req, res) => {
   res.sendFile(path.join(__dirname + '/widget.html'));
+});
+app.get('/advanced', (req, res) => {
+  res.sendFile(path.join(__dirname + '/advanced.html'));
 });
 
 app.listen(port, () => {
