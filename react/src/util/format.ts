ac50a372ea48886c84d111a17e10d5b3708a1468
Unknown <paoolkim@gmail.com>
2021-03-01 13:33:25 -0800

    format Price function

--- react/src/util/format.ts
+++ react/src/util/format.ts
@@ -1,3 +1,6 @@
+import currency from 'currency-formatter';
+import { fiatCurrency } from './api-client';
+
 export const amount = (x?: number | null): string | undefined => {
   const clean = +x!;
   if (clean === 0) return;
@@ -7,6 +10,37 @@ export const amount = (x?: number | null): string | undefined => {
     .replace(/(\.\d*?)0*$/, '$1');
 };
 
+export const formatPrice = (price: number, currencyType: fiatCurrency) => {
+  let symbol;
+
+  switch (currencyType) {
+    case 'EUR':
+      symbol = '€';
+      break;
+    case 'USD':
+      symbol = '$';
+      break;
+    case 'AUD':
+      symbol = '$';
+      break;
+    case 'CAD':
+      symbol = '$';
+      break;
+    case 'GBP':
+      symbol = '£';
+      break;
+  }
+
+  return currency.format(price, {
+    symbol,
+    decimal: '.',
+    thousand: ',',
+    precision: 0,
+    format: '%s%v',
+  });
+};
+
 export default {
   amount,
+  formatPrice,
 };
