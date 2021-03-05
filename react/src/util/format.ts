3dc27d2348463a79957b19e9cbccd8a2017219be
Unknown <paoolkim@gmail.com>
2021-03-05 14:23:59 -0800

    additional format functions

--- react/src/util/format.ts
+++ react/src/util/format.ts
@@ -10,7 +10,11 @@ export const amount = (x?: number | null): string | undefined => {
     .replace(/(\.\d*?)0*$/, '$1');
 };
 
-export const formatPrice = (price: number, currencyType: fiatCurrency) => {
+export const formatPrice = (
+  price: number,
+  currencyType: fiatCurrency,
+  precision = 0,
+) => {
   let symbol;
 
   switch (currencyType) {
@@ -35,12 +39,39 @@ export const formatPrice = (price: number, currencyType: fiatCurrency) => {
     symbol,
     decimal: '.',
     thousand: ',',
-    precision: 0,
+    precision: precision,
     format: '%s%v',
   });
+};
+
+export const formatComma = (number: number) => {
+  const formattedString = currency.format(number, {
+    symbol: '',
+    decimal: '',
+    thousand: ',',
+    precision: 0,
+    format: '%v',
+  });
+
+  return formattedString;
+};
+
+export const formatBCH = (bch: string) => {
+  const val = +bch;
+  const formattedString = currency.format(val, {
+    symbol: '',
+    decimal: '.',
+    thousand: ',',
+    precision: 8,
+    format: '%v',
+  });
+
+  return formattedString;
 };
 
 export default {
   amount,
   formatPrice,
+  formatComma,
+  formatBCH,
 };
