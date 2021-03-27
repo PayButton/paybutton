7ac54c49fe64f4606c929a180fe003bc0ecccb84
Unknown <paoolkim@gmail.com>
2021-03-26 20:28:08 -0700

    replace rest with paybutton server

--- react/src/util/api-client.ts
+++ react/src/util/api-client.ts
@@ -1,11 +1,34 @@
 import axios from 'axios';
 
+// export const getAddressDetails = async (
+//   address: string,
+// ): Promise<AddressDetails> => {
+//   const res = await fetch(
+//     `https://rest.bitcoin.com/v2/address/details/${address}`,
+//   );
+//   return await res.json();
+// };
+
 export const getAddressDetails = async (
   address: string,
 ): Promise<AddressDetails> => {
   const res = await fetch(
-    `https://rest.bitcoin.com/v2/address/details/${address}`,
+    `https://api.paybutton.org/address/transactions/${address}`,
   );
+  return await res.json();
+};
+
+export const getSatoshiBalance = async (
+  address: string,
+): Promise<{ satoshis: number }> => {
+  const res = await fetch(
+    `https://api.paybutton.org/address/balance/${address}`,
+  );
+  return await res.json();
+};
+
+export const getUTXOs = async (address: string): Promise<UtxoDetails> => {
+  const res = await fetch(`https://api.paybutton.org/address/utxo/${address}`);
   return await res.json();
 };
 
@@ -38,20 +61,136 @@ export type fiatCurrency = 'USD' | 'CAD' | 'EUR' | 'GBP' | 'AUD';
 export type cryptoCurrency = 'BCH' | 'SAT' | 'bits';
 export type currency = cryptoCurrency | fiatCurrency;
 
+// export interface AddressDetails {
+//   balance: number;
+//   balanceSat: number;
+//   totalReceived: number;
+//   totalReceivedSat: number;
+//   totalSent: number;
+//   totalSentSat: number;
+//   unconfirmedBalance: number;
+//   unconfirmedBalanceSat: number;
+//   unconfirmedTxAppearances: number;
+//   txAppearances: number;
+//   transactions: [string];
+//   legacyAddress: string;
+//   cashAddress: string;
+// }
 export interface AddressDetails {
-  balance: number;
-  balanceSat: number;
-  totalReceived: number;
-  totalReceivedSat: number;
-  totalSent: number;
-  totalSentSat: number;
-  unconfirmedBalance: number;
-  unconfirmedBalanceSat: number;
-  unconfirmedTxAppearances: number;
-  txAppearances: number;
-  transactions: [string];
-  legacyAddress: string;
-  cashAddress: string;
+  confirmedTransactionsList: [ConfirmedTransaction];
+  unconfirmedTransactionsList: [UnconfirmedTransaction];
+}
+export interface ConfirmedTransaction {
+  hash: string;
+  version: number;
+  inputsList: {
+    index: number;
+    outpoint: {
+      hash: string;
+      index: number;
+    };
+    signatureScript: string;
+    sequence: number;
+    value: number;
+    previousScript: string;
+    address: string;
+  }[];
+  outputsList: {
+    index: number;
+    value: number;
+    pubkeyScript: string;
+    address: string;
+    scriptClass: string;
+    disassembledScript: string;
+  }[];
+  lockTime: number;
+  size: number;
+  timestamp: number;
+  confirmations: number;
+  blockHeight: number;
+  blockHash: string;
+  slpTransactionInfo: {
+    slpAction: number;
+    validityJudgement: number;
+    parseError: '';
+    tokenId: '';
+    burnFlagsList: [];
+  };
+}
+
+export interface UnconfirmedTransaction {
+  transaction: {
+    hash: string;
+    version: number;
+    inputsList: {
+      index: number;
+      outpoint: {
+        hash: string;
+        index: number;
+      };
+      signatureScript: string;
+      sequence: number;
+      value: number;
+      previousScript: string;
+      address: string;
+    }[];
+    outputsList: {
+      index: number;
+      value: number;
+      pubkeyScript: string;
+      address: string;
+      scriptClass: string;
+      disassembledScript: string;
+    }[];
+    lockTime: number;
+    size: number;
+    timestamp: number;
+    confirmations: number;
+    blockHeight: number;
+    blockHash: string;
+    slpTransactionInfo: {
+      slpAction: number;
+      validityJudgement: number;
+      parseError: string;
+      tokenId: string;
+      burnFlagsList: [];
+    };
+  };
+  addedTime: number;
+  addedHeight: number;
+  fee: number;
+  feePerKb: number;
+  startingPriority: number;
+}
+
+export interface UtxoDetails {
+  outputsList: [Output];
+  tokenMetadataList: [TokenMetaData];
+}
+
+export interface Output {
+  outpoint: {
+    hash: string;
+    index: number;
+  };
+  pubkeyScript: string;
+  value: number;
+  isCoinbase: boolean;
+  blockHeight: number;
+}
+
+export interface TokenMetaData {
+  tokenId: string;
+  tokenType: number;
+  v1Fungible: {
+    tokenTicker: string;
+    tokenName: string;
+    tokenDocumentUrl: string;
+    tokenDocumentHash: string;
+    decimals: number;
+    mintBatonHash: string;
+    mintBatonVout: number;
+  };
 }
 
 export interface PriceData {
