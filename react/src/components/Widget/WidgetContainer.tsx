e4565ef517eac4444584d8c83b5c6002c405288a
chedieck <estevao@lobo.dev.br>
2023-09-15 15:28:44 -0300

    fix: don't create two simultaneous amounts

--- react/src/components/Widget/WidgetContainer.tsx
+++ react/src/components/Widget/WidgetContainer.tsx
@@ -22,7 +22,7 @@ import Widget, { WidgetProps } from './Widget';
 import BigNumber from 'bignumber.js';
 
 export interface WidgetContainerProps
-  extends Omit<WidgetProps, 'loading'|'success'|'setNewTxs'|'setCurrencyObject'> {
+  extends Omit<WidgetProps, 'loading'|'success'|'setNewTxs'|'setCurrencyObject'|'setAmount'> {
   active?: boolean;
   amount?: number;
   currency?: currency;
@@ -215,6 +215,7 @@ export const WidgetContainer: React.FC<WidgetContainerProps> = withSnackbar(
           to={to}
           {...widgetProps}
           amount={amount}
+          setAmount={setAmount}
           goalAmount={goalAmount}
           currency={currency}
           animation={animation}
