7f9bef50d9f809c7f008f61f737fb89a5396f53a
chedieck <estevao@lobo.dev.br>
2024-01-07 16:13:39 -0300

    fix: use Widget, not container

--- paybutton/src/index.tsx
+++ paybutton/src/index.tsx
@@ -2,7 +2,6 @@ import camelcase from 'camelcase';
 import { PayButton, PayButtonProps, PaymentDialog, PaymentDialogProps, Widget, WidgetProps } from 'paybutton';
 import { h } from 'preact';
 import { render } from 'preact/compat';
-import WidgetContainer from 'paybutton/dist/components/Widget/WidgetContainer';
 
 declare global {
   interface Window {
@@ -239,7 +238,7 @@ export default {
   renderWidget: (el: HTMLElement, props: WidgetProps) => {
     if (el !== null) {
       validateJSProps(props)
-      render(<WidgetContainer {...props} />, el)
+      render(<Widget {...props} />, el)
     }
   },
   openDialog: (props: PaymentDialogProps) => openDialog(props)
