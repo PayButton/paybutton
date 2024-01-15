521541f1f33d3abff171e86919d87eb5f0fb4498
chedieck <estevao@lobo.dev.br>
2024-01-15 12:31:23 -0300

    feat: change from paybutton$ to paybutton@ and add 9byte secret

--- react/src/util/opReturn.ts
+++ react/src/util/opReturn.ts
@@ -1,4 +1,16 @@
-export const OP_RETURN_PREFIX = 'paybutton$';
+import { lib, enc } from 'crypto-js';
+
+export const OP_RETURN_PREFIX = 'paybutton@';
+// The prefix format is:
+// paybutton@[random 12 base64 chars]$
+// e.g.: paybutton@F8Ulk2rSf3Fj$
+
+// 220 total bytes
+// - 10 from prefix: 'paybutton@'
+// - 9 random bytes for identification (the 12 base64 chars)
+// - 1 from the ending '$'.
+// = 200 available bytes
+export const BYTES_LIMIT = 220 - 10 - 9 - 1; // 200
 
 export function stringToHex(str: string): string {
   return str
@@ -7,14 +19,27 @@ export function stringToHex(str: string): string {
     .join('');
 }
 
+export function generateRandom9ByteString(): string {
+  // Generate 9 random bytes
+  const wordArray = lib.WordArray.random(9);
+
+  // Convert the word array to a Base64 string
+  const base64String = enc.Base64.stringify(wordArray);
+
+  // Return it without the always-present single '=' at the end
+  return base64String;
+}
+
 export function parseOpReturn(
   opReturn: string | undefined,
 ): string | undefined {
   if (opReturn === undefined) return undefined;
   const bytesQuantity = new Blob([opReturn]).size;
 
-  if (bytesQuantity > 70) {
-    throw new Error(`Maximum 70 byte size exceeded: ${bytesQuantity}`);
+  if (bytesQuantity > BYTES_LIMIT) {
+    throw new Error(
+      `Maximum ${BYTES_LIMIT} byte size exceeded: ${bytesQuantity}`,
+    );
   }
-  return OP_RETURN_PREFIX + opReturn;
+  return OP_RETURN_PREFIX + generateRandom9ByteString() + '$' + opReturn;
 }
