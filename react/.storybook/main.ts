bea59865291e6771a4f1ec938b2e52c6dddb5f43
chedieck <estevao@lobo.dev.br>
2024-01-24 12:49:47 -0300

    chore: migrate breaking changes on storybook

--- react/.storybook/main.ts
+++ react/.storybook/main.ts
@@ -1,12 +1,25 @@
-module.exports = {
+import type { StorybookConfig } from "@storybook/react-webpack5";
+
+const config: StorybookConfig = {
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
-    "@storybook/preset-create-react-app",
-  ]
+  ],
+
+  docs: {
+    autodocs: true
+  },
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {babelModeV7: true}
+  }
 };
+
+export default config
