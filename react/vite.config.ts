0507a9a4e142cebec94503d274dbf27b2231a699
lissavxo <lissavitoria12@gmail.com>
2024-02-28 13:23:44 -0300

    chore: build fixes

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import commonjs from 'vite-plugin-commonjs'
+import path from 'path'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: '.',
+    plugins: [
+        react(),
+        commonjs(),
+        dts({ rollupTypes: true })
+    ],
+    build: {
+        outDir: 'dist',
+        lib: {
+        entry: 'index.tsx',
+        fileName: 'index'
+        },
+        rollupOptions: {
+            external: ['react'],
+            output: {
+              globals: {
+                react: 'React',
+              },
+            },
+        },
+    },
 })
