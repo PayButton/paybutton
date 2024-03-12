0cad661eb69e87beb019fcfab86db31e2bc2b03d
lissavxo <lissavitoria12@gmail.com>
2024-03-12 11:33:10 -0300

    fix: build

---
+++ react/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { to } from "../.storybook/default-args";
+import { PayButton } from "../lib/components";
+
+function App() {
+  return (
+    <div>
+      <h1>Paybutton Demo</h1>
+      <PayButton
+      to={to}
+      />
+    </ div>
+  );
+}
+
+export default App;
