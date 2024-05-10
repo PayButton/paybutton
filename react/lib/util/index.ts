f4b4eedd34b98c1e26734c9e6adf60cac5941665
lissavxo <lissavitoria12@gmail.com>
2024-05-10 01:55:11 -0300

    refactor: add currency util

--- react/lib/util/index.ts
+++ react/lib/util/index.ts
@@ -1,29 +1,3 @@
-import { CRYPTO_CURRENCIES, FIAT_CURRENCIES } from './constants';
-import { cryptoCurrency, fiatCurrency } from './types';
-
-export function isFiat(unknownString: string): unknownString is fiatCurrency {
-  return FIAT_CURRENCIES.includes(unknownString);
-}
-
-export function isCrypto(
-  unknownString: string,
-): unknownString is cryptoCurrency {
-  return CRYPTO_CURRENCIES.includes(unknownString);
-}
-
-export function isValidCurrency(
-  unknownString: string,
-): unknownString is cryptoCurrency {
-  return isFiat(unknownString) || isCrypto(unknownString);
-}
-
-export const getCashtabProviderStatus = () => {
-  const windowAny = window as any;
-  if (window && windowAny.bitcoinAbc && windowAny.bitcoinAbc === 'cashtab') {
-    return true;
-  }
-  return false;
-};
 
 export * from './address';
 export * from './api-client';
@@ -34,3 +8,14 @@ export * from './randomizeSats';
 export * from './satoshis';
 export * from './socket';
 export * from './types';
+export * from './number';
+export * from './currency';
+
+
+export const getCashtabProviderStatus = () => {
+  const windowAny = window as any;
+  if (window && windowAny.bitcoinAbc && windowAny.bitcoinAbc === 'cashtab') {
+    return true;
+  }
+  return false;
+};
