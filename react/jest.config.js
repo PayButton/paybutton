58d87ef2dcbb77423372f49367708e89fbab0f38
lissavxo <lissavitoria12@gmail.com>
2024-05-22 00:40:21 -0300

    test: remove coverageThreshold

--- react/jest.config.js
+++ react/jest.config.js
@@ -12,11 +12,6 @@ module.exports = {
     "lib/tests",
     "lib/assets"
   ],
-  coverageThreshold: {
-    "global": {
-      "lines": 80
-    }
-  },
   transformIgnorePatterns: [
     'node_modules/(?!(@babylonjs/core|@babylonjs/loaders)).+.[t|j]sx?$',
   ],
