dc3af61e2b244df919be6cff2befd06f020320ba
lissavxo <lissavitoria12@gmail.com>
2024-05-21 22:21:39 -0300

    chore: set tests coverage

--- react/jest.config.js
+++ react/jest.config.js
@@ -3,4 +3,21 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePaths: ['./'],
+  collectCoverageFrom: [
+    'lib/**/*.{ts,tsx,mjs}',
+  ],
+  coveragePathIgnorePatterns: [
+    "src/*",
+    "lib/themes",
+    "lib/tests",
+    "lib/assets"
+  ],
+  coverageThreshold: {
+    "global": {
+      "lines": 80
+    }
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(@babylonjs/core|@babylonjs/loaders)).+.[t|j]sx?$',
+  ],
 };
