d9715ab570cda4882081711361bc41274f5829c1
chedieck <estevao@lobo.dev.br>
2024-05-23 14:33:19 -0300

    fix: render for react 18

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import * as ReactDOM from 'react-dom';
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById("root")
+const root = ReactDOM.createRoot(container!);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+  </React.StrictMode>
+);
