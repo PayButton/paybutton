12da4d4120545905c6114204d54ad218af901b9f
chedieck <estevao@lobo.dev.br>
2024-05-29 16:40:14 -0300

    feat: remove redundant main.tsx file

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -2,19 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
-import * as packageJson from './package.json'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    dts({ include: ['lib', 'main.tsx'],})
+    dts({ include: ['lib', 'lib/main.ts'],})
   ],
   build: {
     copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, 'main.tsx'),
+      entry: resolve(__dirname, 'lib/main.ts'),
       formats: ['es'],
       fileName: 'app'
     },
