c64133b51c8ace5c6fc0a6c27a5b65081168ed9d
chedieck <estevao@lobo.dev.br>
2024-05-30 12:42:29 -0300

    Revert "feat: remove redundant main.tsx file"
    This reverts commit 12da4d4120545905c6114204d54ad218af901b9f.

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -2,18 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
+import * as packageJson from './package.json'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    dts({ include: ['lib', 'lib/main.ts'],})
+    dts({ include: ['lib', 'main.tsx'],})
   ],
   build: {
     copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
+      entry: resolve(__dirname, 'main.tsx'),
       formats: ['es'],
       fileName: 'app'
     },
