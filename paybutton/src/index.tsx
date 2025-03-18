99d32c030d36657c826ce25551f57c6961f1a84f
lissavxo <lissavitoria12@gmail.com>
2025-03-18 11:31:39 -0300

    feat: transaction text prop

--- paybutton/src/index.tsx
+++ paybutton/src/index.tsx
@@ -100,7 +100,8 @@ const allowedProps = [
   'wsBaseUrl',
   'apiBaseUrl',
   'disableAltpayment',
-  'contributionOffset'
+  'contributionOffset',
+  'transactionText',
 ];
 
 const requiredProps = [
