632d7fc001cfde609d73b0effd96c41c70020a59
lissavxo <lissavitoria12@gmail.com>
2025-05-05 16:39:26 -0300

    refactor: clean code

--- react/lib/components/Widget/Widget.tsx
+++ react/lib/components/Widget/Widget.tsx
@@ -958,7 +958,7 @@ export const Widget: React.FunctionComponent<WidgetProps> = props => {
                     onClick={handleButtonClick}
                     disabled={isPropsTrue(disabled)}
                     animation={animation}
-                    size={size}
+                    size={'medium'}
                   />
                 </Box>
               )}
