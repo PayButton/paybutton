581bbc93129b2903e6379fa6d1b7db4bdbcedad4
David Klakurka <klakurka@gmail.com>
2025-08-03 15:18:36 -0700

    Upgraded to rollup v4, removed yarn-conflicting package-lock.json.

--- paybutton/rollup.prod.js
+++ paybutton/rollup.prod.js
@@ -1,13 +1,13 @@
 import common from './rollup.config.js';
 
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 const config = common( 'production' );
-
-module.exports = config;
 
 config.plugins.push(
   terser( {
     mangle: true,
   } ),
 );
+
+export default config;
